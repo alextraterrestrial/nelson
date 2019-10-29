0ca9cb8766e605b434105156490a366bddc82704
Alex <alex.sallberg@gmail.com>
2019-10-29 23:48:41 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require("header.php")
+?>
+    <main>
+        <?php
+        if(isset($_SESSION["loggedin"])){
+            require("welcome.html");
+        } else{
+            require("login.php");
+            
+        }
+
+        ?>
+    </main>
+
+<?php
+require("footer.php")
+?>
