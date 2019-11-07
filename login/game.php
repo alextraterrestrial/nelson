9604094540158666cb4a81edf1039e4c7a83cf24
Alex <alex.sallberg@gmail.com>
2019-11-07 13:52:17 +0100

    Adding game.php

---
+++ login/game.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Nelson</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="cssHeader.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Overpass+Mono&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+      <?php include("headerNav.html")?>
+      <section id="secTeam">team</section>
+      <section id="secSpellogg">spellog</section>
+      <script src="jquery-3.4.1.min.js"></script>
+      <script src="auxFunctions.js"></script>
+      <script src="header.js"></script>
+    </body>
+  
+  </html>
