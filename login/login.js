e1a1cba545f8d8abe80a418043d730f3024daa80
Alex <alex.sallberg@gmail.com>
2019-11-19 00:07:39 +0100

    Adding to the js login

--- login/login.js
+++ login/login.js
@@ -1,21 +1,37 @@
-class user {
-    constructor() {
-        this.id;
-        this.userName;
-    }
+$('document').ready(function() {
+    let request;
 
-    get userName() {
+    // Validate form
+    $("form[name='loginForm']").validate({
+        //Validation rules
+        rules: {
+            email: "required",
+            password: "required"
+        },
+        messages: {
+            email: "Vänligen ange en mailadress",
+            password: "Vänligen ange ett lösenord"
+        },
+        submitHandler: form => {
+            let email = form.email.value;
+            let password = form.password.value;
+            let formData = { email: email, password: password };
+            formData = JSON.stringify(formData);
+            console.log(formData)
 
-    }
-
-}
-
-
-
-const loginValidation = () => {
-    //Prevent submit
-    $("#login").preventDefault();
-
-    //Validate email
-
-}
+            request = $.ajax({
+                    url: "../login/login.php",
+                    type: "POST",
+                    data: formData,
+                    encode: true
+                })
+                .done((res) => {
+                    console.log("Success")
+                    console.log(res)
+                })
+                .fail(() => {
+                    console.log("Fail!")
+                })
+        }
+    })
+})
--- login/login.php
+++ login/login.php
@@ -1,15 +1,17 @@
 <?php
+//Get form data
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+
+$test = json_encode("Alex");
+echo $email;
+
 //Show server errors 'on'
 ini_set('display_errors', 'on');
 
 // Initialize the session
 session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-// if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-//     header("location: welcome.php");
-//     exit;
-// }
  
 // Include config file
 require_once "config.php";
@@ -20,124 +22,64 @@ $email_err = $password_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["email"]))){
-        $email_err = "Vänligen ange din mailadress";
-    } else{
-        $email = trim($_POST["email"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Vänligen ange ditt lösenord";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
     // Validate credentials
-    if(empty($email_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, email, password FROM users WHERE email = :email";
+
+    // Prepare a select statement
+    $sql = "SELECT id, email, password FROM users WHERE email = :email";
+    
+    if($stmt = $pdo->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
         
-        if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
-            
-            // Set parameters
-            $param_email = trim($_POST["email"]);
-            
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Check if username exists, if yes then verify password
-                if($stmt->rowCount() == 1){
-                    if($row = $stmt->fetch()){
-                        $id = $row["id"];
-                        $username = $row["email"];
-                        $hashed_password = $row["password"];
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            // session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["email"] = $email;                            
-                            
-                            // Redirect user to welcome page
-                            // header("location: ../game/game.php");
+        // Set parameters
+        $param_email = trim($_POST["email"]);
+        
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            // Check if username exists, if yes then verify password
+            if($stmt->rowCount() == 1){
+                if($row = $stmt->fetch()){
+                    $id = $row["id"];
+                    $username = $row["email"];
+                    $hashed_password = $row["password"];
+                    if(password_verify($password, $hashed_password)){
+                        // Password is correct, so start a new session
+                        // session_start();
+                        
+                        // Store data in session variables
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["id"] = $id;
+                        $_SESSION["email"] = $email;                            
 
-                            //Create a cookie for the loged in user
+                        //Create a cookie for the loged in user
 
-                            $cookieName = "user";
-                            $cookie = new stdClass();
-                            $cookie -> userId = $id;
-                            $cookie -> logedIn = true;
-                            $cookieValue = json_encode($cookie);
-                            setCookie($cookieName, $cookieValue, 0);
-                            echo $cookieValue;
-                            
-                           
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "Lösenordet du angav var fel";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $email_err = "Inget konto med mailadressen du angav fanns";
+                        $cookieName = "user";
+                        $cookie = new stdClass();
+                        $cookie -> userId = $id;
+                        $cookie -> logedIn = true;
+                        $cookieValue = json_encode($cookie);
+                        setCookie($cookieName, $cookieValue, 0);
+                        echo $cookieValue;
+ 
+                    } 
                 }
             } else{
-                echo "Ojdå, något gick fel...";
+                // Display an error message if username doesn't exist
+                $email_err = "Inget konto med mailadressen du angav fanns";
             }
+        } else{
+            echo "Ojdå, något gick fel...";
         }
-        
-        // Close statement
-        unset($stmt);
     }
     
+    // Close statement
+    unset($stmt);
+
     // Close connection
     unset($pdo);
 }
 ?>
  
-
-<!-- <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head> -->
-
-
-    <div class="wrapper container">
-        <div class="row justify-content-center py-4 my-4">
-            <div class="col-sm-8 col-md-6 col-lg-auto p-4">
-                <h2>Login</h2>
-            
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                    <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                        <label>Email</label>
-                        <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
-                        <span class="help-block"><?php echo $email_err; ?></span>
-                    </div>    
-                    <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                        <label>Lösenord</label>
-                        <input type="password" name="password" class="form-control">
-                        <span class="help-block"><?php echo $password_err; ?></span>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-primary" value="Login">
-                    </div>
-                    <p>Har du inget konto? <a href="">Registrera dig här</a>.</p>
-                </form>
-            </div>
-        </div>
-    </div>
-
 
 
 
