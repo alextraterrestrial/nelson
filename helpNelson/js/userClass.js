14b6224cd622fd1d53ee147ff3367906984f871c
simonahlstrom <54178540+simonahlstrom@users.noreply.github.com>
2020-01-17 11:56:52 +0100

    user functionality

--- helpNelson/js/userClass.js
+++ helpNelson/js/userClass.js
@@ -1,5 +1,26 @@
 class User {
-  constructor() {
-    
+  // constructor
+  constructor(obj) {
+    this.id = obj.id
+    this.username = obj.username
+    this.password = obj.password
+    this.email = obj.email
+    this.score = obj.score
+    this.dateReg = obj.dateReg
+    this.teamId = obj.teamId
+    this.teamName = obj.teamName
+    this.status = obj.status
+  }
+
+  // methods
+  submitAnswer(submission) {
+    // takes the submission and checks/uploads to DB, handling team funcitonality
+  }
+}
+
+class Captain extends User {
+  // methods
+  adminUser(action) {
+    // targets a user and adds, removes or makes into captain depending on the passed action
   }
 }
---
+++ helpNelson/js/userFunctionality.js
@@ -0,0 +1,3 @@
+function createUser(id, password) {
+  
+}
