442acc0d344d776e206f06cb50694a31eaf7cc55
simonahlstrom <54178540+simonahlstrom@users.noreply.github.com>
2020-01-20 10:39:17 +0100

    updated getUserInfo.php

--- helpNelson/getUserInfo.php
+++ helpNelson/getUserInfo.php
@@ -14,7 +14,48 @@ $sql->bindParam(2, $_GET['password']);
 $sql->execute();
 $answer = $sql->fetchAll(\PDO::FETCH_ASSOC);
 
-$answer = json_encode($answer);
-echo $answer;
+if ($answer) {
+  // $query = "SELECT User.*, Team.teamId, Team.teamName, UserTeam.status FROM User 
+  // JOIN UserTeam ON User.userId = UserTeam.userId 
+  // JOIN Team ON UserTeam.teamId = Team.teamId
+  // WHERE User.userId = ? AND User.password = ?";
+
+  // $sql = $pdo->prepare($query);
+  // $sql->bindParam(1, $_GET['id']);
+  // $sql->bindParam(2, $_GET['password']);
+  // $sql->execute();
+  // $answer = $sql->fetchAll(\PDO::FETCH_ASSOC);
+
+  $answer = json_encode($answer);
+  echo $answer;
+
+} else {
+  $query = "SELECT User.* WHERE User.userId = ? AND User.password = ?";
+
+  $sql = $pdo->prepare($query);
+  $sql->bindParam(1, $_GET['id']);
+  $sql->bindParam(2, $_GET['password']);
+  $sql->execute();
+  $answer = $sql->fetchAll(\PDO::FETCH_ASSOC);
+
+  $answer = json_encode($answer);
+  echo $answer;
+}
+
+
+
+// $query = "SELECT User.*, Team.teamId, Team.teamName, UserTeam.status FROM User 
+// JOIN UserTeam ON User.userId = UserTeam.userId 
+// JOIN Team ON UserTeam.teamId = Team.teamId
+// WHERE User.userId = ? AND User.password = ?";
+
+// $sql = $pdo->prepare($query);
+// $sql->bindParam(1, $_GET['id']);
+// $sql->bindParam(2, $_GET['password']);
+// $sql->execute();
+// $answer = $sql->fetchAll(\PDO::FETCH_ASSOC);
+
+// $answer = json_encode($answer);
+// echo $answer;
 
 ?>
