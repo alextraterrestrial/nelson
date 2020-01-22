c94d8dd655ac529501f2b3c0fced201145be7110
LinusGrahn <56296755+LinusGrahn@users.noreply.github.com>
2020-01-22 12:38:10 +0100

    linus

--- helpNelson/php/config.php
+++ helpNelson/php/config.php
@@ -1,19 +1,19 @@
 <?php
     // Database credentials
-    // define('DB_SERVER', 'localhost:8889');
-    // define('DB_USERNAME', 'root');
-    // define('DB_PASSWORD', 'root');
-    // define('DB_NAME', '244914-helpnelson');
-
-    define('DB_SERVER', 'my88b.sqlserver.se');
-    define('DB_USERNAME', '244914_xd85104');
-    define('DB_PASSWORD', 'AllHailNelson666');
+    define('DB_SERVER', 'localhost:8889');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'root');
     define('DB_NAME', '244914-helpnelson');
+
+    // define('DB_SERVER', 'my88b.sqlserver.se');
+    // define('DB_USERNAME', '244914_xd85104');
+    // define('DB_PASSWORD', 'AllHailNelson666');
+    // define('DB_NAME', '244914-helpnelson');
     
     /* Attempt to connect to MySQL database */
     try{
         // $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-        $pdo = new PDO('mysql:host=my88b.sqlserver.se;dbname=244914-helpnelson','244914_xd85104','AllHailNelson666');
+        $pdo = new PDO('mysql:host=localhost;dbname=244914-helpnelson','root','root');
         // Set the PDO error mode to exception
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
        
