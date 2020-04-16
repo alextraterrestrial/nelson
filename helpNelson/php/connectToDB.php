026d08ca389ef5d5bc99c0b8d12a48bd5ec3bd5c
LinusGrahn <56296755+LinusGrahn@users.noreply.github.com>
2020-04-16 11:32:41 +0200

    linus

--- helpNelson/php/connectToDB.php
+++ helpNelson/php/connectToDB.php
@@ -3,8 +3,8 @@
 function getPDO() {
   // har ändrat till mitt DBnamn, bara att ändra tillbaka
   // return new PDO('mysql:host=localhost;dbname=newnelson','root','root');
-  return new PDO('mysql:host=localhost;dbname=244914-helpnelson','root','root');
-  // return new PDO('mysql:host=my56b.sqlserver.se;dbname=246642-helperik','246642_dh39215','AllHailErik666');
+  // return new PDO('mysql:host=localhost;dbname=244914-helpnelson','root','root');
+  return new PDO('mysql:host=my56b.sqlserver.se;dbname=246642-helperik','246642_dh39215','AllHailErik666');
   // return new PDO('mysql:host=localhost;dbname=helperikny','root','root');
   // return new PDO('mysql:host=my88b.sqlserver.se;dbname=244914-helpnelson','244914_xd85104','AllHailNelson666');
 
